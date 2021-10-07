78e15da12a1756f39bfa87e381f31fa0c3c0024b
Nikita <n1ktrnv@gmail.com>
2021-10-08 00:59:35 +0300

    Корректирует имя файла результирующей матрицы

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def _main():
     matrix1 = Matrix.read(f'{FILENAME_BASE}_1.csv')
     matrix2 = Matrix.read(f'{FILENAME_BASE}_2.csv')
     result_matrix = matrix1.multiply(matrix2, f'{FILENAME_BASE}_tmp.csv')
-    result_matrix.write(f'result_{FILENAME_BASE}_result.csv')
+    result_matrix.write(f'{FILENAME_BASE}_result.csv')
 
 
 if __name__ == '__main__':
