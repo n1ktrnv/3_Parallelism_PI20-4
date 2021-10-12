caddeda0fe5bf547956bfa0ed6fe489c0f30254a
Nikita <n1ktrnv@gmail.com>
2021-10-12 16:25:11 +0300

    Добавляет очистку и генерацию папки с матрицами matrices

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-import matrix
-from multiprocessing import Process, Event
+from multiprocessing import Process,  Event
+import shutil
+from pathlib import Path
 
 import keyboard
 
+import matrix
 
+MATRICES_DIR = 'matrices'
 FILENAME_BASE = 'matrix'
 STOP_KEY = 'x'
 
@@ -12,17 +15,23 @@ def task(n, min_value, max_value, stop):
     count = 1
     while not stop.is_set():
         first_matrix = matrix.sqr_random(n, min_value, max_value)
-        first_matrix.write(f'matrices/{count}_{FILENAME_BASE}_first.csv')
+        first_matrix.write(f'{MATRICES_DIR}/{count}_{FILENAME_BASE}_first.csv')
         second_matrix = matrix.sqr_random(n, min_value, max_value)
-        second_matrix.write(f'matrices/{count}_{FILENAME_BASE}_second.csv')
+        second_matrix.write(f'{MATRICES_DIR}/{count}_{FILENAME_BASE}_second.csv')
         result_matrix = first_matrix.multiply(
-            second_matrix, f'matrices/{count}_{FILENAME_BASE}_temp.csv'
+            second_matrix, f'{MATRICES_DIR}/{count}_{FILENAME_BASE}_temp.csv'
         )
-        result_matrix.write(f'matrices/{count}_{FILENAME_BASE}_result.csv')
+        result_matrix.write(f'{MATRICES_DIR}/{count}_{FILENAME_BASE}_result.csv')
         count += 1
 
 
+def recreate_matrices_dir():
+    shutil.rmtree(MATRICES_DIR)
+    Path(MATRICES_DIR).mkdir()
+
+
 def _main():
+    recreate_matrices_dir()
     stop = Event()
     n = int(input('Введите размерность матриц: '))
     min_value = int(input('Введите минимальный элемент матриц: '))
